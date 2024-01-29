6ac5232d1052a1cd9b655eeb07ea7c9109689ffe
Dylan Wicker <dylanwicker@Dylans-MacBook-Air.local>
2024-01-29 12:21:36 -0500

    added query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import Navigations from './components/Navigations';
 import { useNavigate } from 'react-router-dom';
 import { fetchAlbums, fetchSpotifyToken } from './fetching';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
 
 function App() {
   const [token, setToken] = useState("")
@@ -13,6 +21,8 @@ function App() {
       <div>
         <h1 className="name"><a href="/"><img className='logo-image' src={"https://pngimg.com/uploads/vinyl/vinyl_PNG14.png"}/></a>Spins</h1>
         <Navigations token={token} setToken={setToken} setSpotifyToken={setSpotifyToken} spotifyToken={spotifyToken}/>
+        <QueryClientProvider client={queryClient}>
+        </QueryClientProvider>
       </div>
     )
   }
